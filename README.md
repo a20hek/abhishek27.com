5394c8d43b78cc4f7291bf4a12b9f736c0b97c36
abhishek <abbhishekk27@gmail.com>
2024-01-22 04:40:24 +0530

    refactor: port to astro

---
+++ README.md
@@ -0,0 +1,1 @@
+# abhwshek.com
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: 'Inter',
+			serif: 'Cormorant Garamond'
+		},
+		screens: {
+			md: '900px'
+		},
+		extend: {}
+	},
+	plugins: []
+}
